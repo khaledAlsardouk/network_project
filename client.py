c836e94beffd66f8b255d11a832973a16306aa3d
khaled <khaledsardouk@gmail.com>
2021-05-12 01:51:11 +0300

    minor changes + score system

--- client.py
+++ client.py
@@ -1,25 +1,35 @@
 import socket
-ClientSocket = socket.socket() #create socket
-host = '127.0.0.1'  #local host ip for now
+import time
+
+ClientSocket = socket.socket()  # create socket
+host = '127.0.0.1'  # local host ip for now
 port = 1233
+score = 0
 
 print('Waiting for connection')
 try:
-    ClientSocket.connect((host, port)) #connect socket to host on  a specific port
-except socket.error as e: # return errors related to socket or address semantics
+    ClientSocket.connect((host, port))  # connect socket to host on  a specific port
+except socket.error as e:  # return errors related to socket or address semantics
     print(str(e))
 
-Response = ClientSocket.recv(1024) # wait and recieve data from server with max size 1024 bytes
-print(Response.decode('ascii')) #decode and print message
+Response = ClientSocket.recv(1024)  # wait and receive data from server with max size 1024 bytes
+print(Response.decode('ascii'))  # decode and print message
 while True:
     Response = ClientSocket.recv(1024)
     print(Response.decode('ascii'))
-    if Response.decode('ascii') != 'you are the attacker': 
+    if Response.decode('ascii') != 'you are the attacker':
         Response = ClientSocket.recv(1024)
-        print(Response)                                 #defender behavior
+        response = Response.decode('ascii')  # defender behavior
         print(Response.decode('ascii'))
-    else:
-        Input = 'attack'                     #attack behavior
-        ClientSocket.send(Input.encode(encoding='ascii'))
+        if response.__eq__('attack'):
+            print('defence failed')
+        else:
+            score = score + 1
+            print('defence successful')
+            print(f'score: {score}')
 
-ClientSocket.close()   #close connection
+    else:
+        time.sleep(4)  # attack behavior time.sleep for testing
+        ClientSocket.send(str.encode('attack', encoding='ascii'))
+
+ClientSocket.close()  # close connection
--- server.py
+++ server.py
@@ -1,40 +1,41 @@
 import socket
 from dotenv import load_dotenv
 
-ServerSocket = socket.socket() #create socket
+ServerSocket = socket.socket()  # create socket
 host = '127.0.0.1'
 port = 1233
 try:
-    ServerSocket.bind((host, port)) #create tcp socket
-except socket.error as e:   # return errors related to socket or address semantics
+    ServerSocket.bind((host, port))  # create tcp socket
+except socket.error as e:  # return errors related to socket or address semantics
     print(str(e))
 
 print('Waiting for a Connection..')
-ServerSocket.listen(5) #wait for a connection
+ServerSocket.listen(5)  # wait for a connection
 
 
 def clients_connection(socket1, socket2):
-    socket1.send(str.encode('you are the attacker', encoding='ascii'))      #assign roles
+    socket1.send(str.encode('you are the attacker', encoding='ascii'))  # assign roles
     socket2.send(str.encode('you are the defender', encoding='ascii'))
-    hostname = socket1.getpeername() #get the attacker address 
-    data = socket1.recv(2048) #wait and recieve data from the attack max 2048 bytes
-    reply = hostname[0] + ':' + str(hostname[1]) + ' says ' + data.decode('ascii') #create a reply
-    print(reply)
-    socket2.sendall(str.encode(reply)) #send the reply to the defender
+    hostname = socket1.getpeername()  # get the attacker address
+    data = socket1.recv(1024)  # wait and receive data from the attack max 2048 bytes
+    reply_origin = hostname[0] + ':' + str(hostname[1]) + ' says ' + data.decode('ascii')  # create a reply
+    reply = data.decode('ascii')
+    print(reply_origin)
+    socket2.sendall(str.encode(reply))  # send the reply to the defender
 
 
 clients = []
 while True:
-    if len(clients) < 2:    #we want only 2 players so 2 connections and 2 clients in the array
-        Client, address = ServerSocket.accept() #accept connection and assign variables 
-        print('Connected to: ' + address[0] + ':' + str(address[1])) #tells us which socket is connected now
-        Client.send(str.encode('Welcome to the Server \nwaiting for the other player')) #send welcome message 
-        clients.append(Client) #add the connection to the array to access it later
+    if len(clients) < 2:  # we want only 2 players so 2 connections and 2 clients in the array
+        Client, address = ServerSocket.accept()  # accept connection and assign variables
+        print('Connected to: ' + address[0] + ':' + str(address[1]))  # tells us which socket is connected now
+        Client.send(str.encode('Welcome to the Server \nwaiting for the other player'))  # send welcome message
+        clients.append(Client)  # add the connection to the array to access it later
 
     else:
-        clients_connection(clients[0], clients[1])  #go to  game
-        clients_connection(clients[1], clients[0]) #simple method to switch turns for now
+        clients_connection(clients[0], clients[1])  # go to  game
+        clients_connection(clients[1], clients[0])  # simple method to switch turns for now
 
 clients[0].close()
-clients[1].close()              #close the connections 
+clients[1].close()  # close the connections
 ServerSocket.close()

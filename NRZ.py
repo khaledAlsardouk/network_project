48393e519f7d709cb9a42a6bf26b6c18989e3b13
Mohammad Kreidieh <master_nitrodox7@outlook.com>
2021-05-14 16:23:35 +0300

    Uploaded NRZ
    added  NRZ.py in a separate file, didnt add it to the code

---
+++ NRZ.py
@@ -0,0 +1,17 @@
+def NRZ(word):
+    #word = "0111000110011"
+    word_in_list= list(word)
+    print(word_in_list)
+    NRZ_of_word=[]
+    for i in word_in_list:
+        if i == '0':
+            flip_the_bit= i.replace("0","1")
+        elif i == '1':
+            flip_the_bit = i.replace("1","0")
+        NRZ_of_word.append(flip_the_bit)
+    y = ''.join(str(j) for j in NRZ_of_word)
+    print (y)
+
+x = input("please enter a binary number to perform NRZ on it:")
+NRZ(x)
+

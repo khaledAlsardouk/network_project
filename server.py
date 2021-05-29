632f2b5c6656f9df7290288145fcdca2d2a668d4
khaled <khaledsardouk@gmail.com>
2021-05-29 19:44:51 +0300

    cleaning

--- server.py
+++ server.py
@@ -19,15 +19,18 @@ clients = []
 
 
 def clients_connection(socket1, socket2):
+    #insert initial time
     socket1.send(str.encode('you are the attacker', encoding='ascii'))  # assign roles
     socket2.send(str.encode('you are the defender', encoding='ascii'))
     hostname = socket1.getpeername()  # get the attacker address
     data = socket1.recv(1024)  # wait and receive data from the attack max 1024 bytes
+    #time when attack is received
     hostname2 = socket1.getpeername()  # get the defender address
     reply_origin = hostname[0] + ':' + str(hostname[1]) + ' says ' + data.decode('ascii')  # create a reply
     print(reply_origin)
     socket2.sendall(data)  # send the reply to the defender
     data2 = socket2.recv(1024)  # see if the defence failed or not
+    #time when defence failed or or successful is received
     reply_origin = hostname2[0] + ':' + str(hostname2[1]) + ' says ' + data2.decode('ascii')  # create a reply
     print(reply_origin)
     socket1.sendall(data2)  # send the response to the attacker
